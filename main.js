6119400439750942e51cf794de8f3dd482a74f06
mccarthygalaxy <124395340+mccarthygalaxy@users.noreply.github.com>
2023-07-08 21:26:40 -0400

    TicketU4_03 (further) fixed URL problem with "mens & womens clothing" endpoints

--- main.js
+++ main.js
@@ -2,27 +2,15 @@
 const baseURL = `https://fakestoreapi.com`;
 const productsAll = "/products"
 const productsJewelry = "/products/category/jewelery"
-const productsMens = "/products/category/men's_clothing"
+const productsMens = "/products/category/men's%20clothing"
 const productsElectronics = "/products/category/electronics"
+const productsWomens = "/products/category/women's%20clothing"
 
-let buildURL = `${baseURL}` + productsAll;
+// let buildURL = `${baseURL}` + productsAll;
 let jewelryURL = `${baseURL}` + productsJewelry;
 let electronicsURL = `${baseURL}` + productsElectronics;
-
 let mensURL = `${baseURL}` + productsMens;
-
-
-let grabFetch = fetch(buildURL)
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-        
-console.log(grabFetch);
-
-let grabFetch2= fetch(jewelryURL)
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-        
-console.log(grabFetch2);
+let womensURL = `${baseURL}` + productsWomens;
 
 let grabFetch3= fetch(mensURL)
             .then(res=>res.json())
@@ -30,8 +18,27 @@ let grabFetch3= fetch(mensURL)
         
 console.log(grabFetch3);
 
-let grabFetch4= fetch(electronicsURL)
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+// let grabFetch = fetch(buildURL)
+// .then(res=>res.json())
+// .then(json=>console.log(json))
+
+// console.log(grabFetch);
+
+
+// let grabFetch2= fetch(jewelryURL)
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
         
-console.log(grabFetch4);
+// console.log(grabFetch2);
+
+// let grabFetch4= fetch(electronicsURL)
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
+
+// console.log(grabFetch4);
+
+// let grabFetch5= fetch(womensURL)
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
+        
+// console.log(grabFetch5);

9c25af03c2c037c289b94891c251007495621ee8
mccarthygalaxy <124395340+mccarthygalaxy@users.noreply.github.com>
2023-07-09 16:04:20 -0400

    Ticket U4_03 Complete; fakeStore(endpoint) function working, heroContainer delivering data from API to DOM correctly. Next working on DISPLAY div to populate section with all cards from given category.

--- main.js
+++ main.js
@@ -1,44 +1,116 @@
 //! Global Variables
 const baseURL = `https://fakestoreapi.com`;
 const productsAll = "/products"
+const productsElectronics = "/products/category/electronics"
 const productsJewelry = "/products/category/jewelery"
 const productsMens = "/products/category/men's%20clothing"
-const productsElectronics = "/products/category/electronics"
 const productsWomens = "/products/category/women's%20clothing"
 
-// let buildURL = `${baseURL}` + productsAll;
+let buildURL = `${baseURL}` + productsAll;
 let jewelryURL = `${baseURL}` + productsJewelry;
 let electronicsURL = `${baseURL}` + productsElectronics;
 let mensURL = `${baseURL}` + productsMens;
 let womensURL = `${baseURL}` + productsWomens;
 
-let grabFetch3= fetch(mensURL)
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-        
-console.log(grabFetch3);
+let cart = []; // array for items in shopping cart
+let grouping = []; // current category of cards
 
-// let grabFetch = fetch(buildURL)
-// .then(res=>res.json())
-// .then(json=>console.log(json))
+const catAll = document.getElementById('catAll');
+const catJewelry = document.getElementById('catJewelry');
+const catElectronics = document.getElementById('catElectronics');
+const catMens = document.getElementById('catMens');
+const catWomens = document.getElementById('catWomens');
 
-// console.log(grabFetch);
+const heroContainer = document.querySelector('.heroContainer');
 
 
-// let grabFetch2= fetch(jewelryURL)
-//             .then(res=>res.json())
-//             .then(json=>console.log(json))
-        
-// console.log(grabFetch2);
+//! Anonymous Functions
 
-// let grabFetch4= fetch(electronicsURL)
-//             .then(res=>res.json())
-//             .then(json=>console.log(json))
+const displayFirstCard = item => {
+    console.log(item);
 
-// console.log(grabFetch4);
+    //* Create Elements
+    let card = document.createElement('div');
+    let img = document.createElement('img');
+    let body = document.createElement('div');
+    let title = document.createElement('h5');
+    let description = document.createElement('p');
+    let price = document.createElement('h5');
+    let btn = document.createElement('btn');
 
-// let grabFetch5= fetch(womensURL)
-//             .then(res=>res.json())
-//             .then(json=>console.log(json))
-        
-// console.log(grabFetch5);
+    //* Set Attributes
+    card.className = 'card';
+    card.style.width = '18rem';
+    img.src = item.img;
+    img.className = 'card-img-top';
+    img.alt = item.title;
+    img.title = item.title;
+    description.className = 'card-text';
+
+    // img.style.height = "100px";
+
+    body.className = 'card-body';
+    title.textContent = item.title;
+    description.textContent = item.desc;
+    price.className = 'card-price';
+    btn.className = 'btn btn-primary';
+    btn.textContent = 'Add To Cart';
+
+    //* Attach/Append Elements
+    body.appendChild(title);
+    body.appendChild(description);
+    body.appendChild(btn);
+    
+    card.appendChild(img);
+    card.appendChild(body);
+
+    heroContainer.appendChild(card);
+
+}
+
+//! Listening Events
+
+catAll.addEventListener('click', () => {
+    fakeStore(productsAll);
+})
+
+catJewelry.addEventListener('click', () => {
+    fakeStore(productsJewelry); // Call the fakeStore (fetchItems) function with the corresponding category URL
+});
+
+catElectronics.addEventListener('click', () => {
+    fakeStore(productsElectronics);
+});
+
+catMens.addEventListener('click', () => {
+    fakeStore(productsMens);
+});
+
+catWomens.addEventListener('click', () => {
+    fakeStore(productsWomens);
+})
+
+//! Function to Fetch based on Category
+function fakeStore(endpoint) {  // FakeStore Function
+    fetch(baseURL + endpoint)
+        .then((res) => res.json())
+        .then((data) => {           // Process the fetched items data
+            // console.log(data);
+            let singleItem = data[0];
+            // console.log(singleItem);
+
+            let singleObj = {
+                title: singleItem.title,
+                desc: singleItem.description,
+                price: singleItem.price,
+                img: singleItem.image,
+                category: singleItem.category
+            }
+            // console.log(singleObj);
+
+            displayFirstCard(singleObj);
+        })
+        .catch((err) => {
+            console.error('Error:', err);
+        });
+}
